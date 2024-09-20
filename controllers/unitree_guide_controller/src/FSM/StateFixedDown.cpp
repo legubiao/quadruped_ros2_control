956b684c8b32851faa1db9982cc8f769e51a3556
Huang Zhenbiao <biao876990970@hotmail.com>
2024-09-20 15:29:44 +0800

    fix go1 gazebo visualization

--- controllers/unitree_guide_controller/src/FSM/StateFixedDown.cpp
+++ controllers/unitree_guide_controller/src/FSM/StateFixedDown.cpp
@@ -26,8 +26,8 @@ void StateFixedDown::run() {
             phase * target_pos_[i] + (1 - phase) * start_pos_[i]);
         ctrl_comp_.joint_velocity_command_interface_[i].get().set_value(0);
         ctrl_comp_.joint_torque_command_interface_[i].get().set_value(0);
-        ctrl_comp_.joint_kp_command_interface_[i].get().set_value(50.0);
-        ctrl_comp_.joint_kd_command_interface_[i].get().set_value(3.5);
+        ctrl_comp_.joint_kp_command_interface_[i].get().set_value(30.0);
+        ctrl_comp_.joint_kd_command_interface_[i].get().set_value(1.5);
     }
 }
 
--- hardwares/hardware_unitree_mujoco/README.md
+++ hardwares/hardware_unitree_mujoco/README.md
@@ -1,5 +1,9 @@
 # Hardware Unitree Mujoco
 
+This package contains the hardware interface based on [unitree_sdk2](https://github.com/unitreerobotics/unitree_sdk2) to control the Unitree robot in Mujoco simulator. 
+
+In theory, it also can communicate with real robot, but it is not tested yet. You can use go2 simulation in [unitree_mujoco](https://github.com/unitreerobotics/unitree_mujoco).
+
 * build
 ```bash
 cd ~/ros2_ws
