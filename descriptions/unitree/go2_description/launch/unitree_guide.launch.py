a17b9448c3df5b1cd6f9961be6f75e4447fe790b
Zhenbiao Huang <biao876990970@hotmail.com>
2024-10-12 00:56:49 +0800

    fix launch for humble

--- descriptions/unitree/go2_description/launch/unitree_guide.launch.py
+++ descriptions/unitree/go2_description/launch/unitree_guide.launch.py
@@ -37,6 +37,9 @@ def generate_launch_description():
         executable="ros2_control_node",
         parameters=[robot_controllers],
         output="both",
+        remappings=[
+            ("~/robot_description", "/robot_description"),
+        ],
     )
 
     robot_state_publisher = Node(
